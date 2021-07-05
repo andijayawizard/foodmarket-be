f98956611068f8a480c31f7c770217fe39dd1b6b
andijayawizard <33024329+andijayawizard@users.noreply.github.com>
2021-07-05 17:46:43 +0700

    0507

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\API\MidtransController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// homepage
+Route::get('/', function () {
+    // return redirect()->route('dashboard');
+    return view('welcome');
+});
+
+// dashboard
+// Route::prefix('dashboard')
+//     ->middleware(['auth:sanctum', 'admin'])
+//     ->group(function() {
+//         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+//     });
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+// midtrans
+Route::get('midtrans/success', [MidtransController::class, 'success']);
+Route::get('midtrans/unfinish', [MidtransController::class, 'unfinish']);
+Route::get('midtrans/error', [MidtransController::class, 'error']);
