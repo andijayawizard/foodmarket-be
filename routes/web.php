8bc009aca86815b1465e8deeb2d00ea1d1a12b5d
andijayawizard <andijayawizard@gmail.com>
2022-07-25 18:02:08 +0700

    2507

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\API\MidtransController;
 
 /*
@@ -24,8 +25,9 @@ Route::get('/', function () {
 // dashboard
 // Route::prefix('dashboard')
 //     ->middleware(['auth:sanctum', 'admin'])
-//     ->group(function() {
+//     ->group(function () {
 //         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+//         Route::resource('users', UserController::class);
 //     });
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
